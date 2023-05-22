58d4b8c8fb449aa0684413d9d9f5323e435262fa
healixdev <134302698+healixdev@users.noreply.github.com>
2023-05-22 20:03:23 +0100

    Update main.js

--- pb/main.js
+++ pb/main.js
@@ -1,5 +1,8 @@
 
+let sound = new Audio('./The Rolling Stones - Paint It Black.mp3');
+
 function black(key){
+    sound.play();
     switch(key){
         case "Adolf":
             document.getElementById(key).src = ("./assets/" + String(key) + "Black.jpg");
@@ -7,8 +10,8 @@ function black(key){
         case "Doc":
             document.getElementById(key).src = ("./assets/" + String(key) + "Black.jpg");
             break;
-            case "PM":
-                document.getElementById(key).src = ("./assets/" + String(key) + "Black.jpg");
-                break;
+         case "PM":
+             document.getElementById(key).src = ("./assets/" + String(key) + "Black.jpg");
+             break;
     }
 }
